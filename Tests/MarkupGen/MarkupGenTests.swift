7ba473d674973134bd47df0237e83721045179b6
Ankit Aggarwal <ankit.spd@gmail.com>
2016-06-08 14:17:40 +0530

    Add test for generic function

--- Tests/MarkupGen/MarkupGenTests.swift
+++ Tests/MarkupGen/MarkupGenTests.swift
@@ -26,6 +26,12 @@ class MarkupGenTests: XCTestCase {
         XCTAssertEqual(gen, theFunc)
     }
 
+    func testGenericFunction() throws {
+        let gen = try parseFunction(funcString: "@noreturn public func hello<T: where B: A>(a: Int, b: T, c: (AType)->()) throws -> T {}")
+        let theFunc = Function(name: "hello(a:b:c:)", params: ["a", "b", "c"], returns: true, throws: true) 
+        XCTAssertEqual(gen, theFunc)
+    }
+
     func testLabels() throws {
         let gen = try parseFunction(funcString: "func hello(_ a: Int, boo b: Double, cool: Type) throws -> CoolType {}")
         let theFunc = Function(name: "hello(_:boo:cool:)", params: ["a", "boo", "cool"], returns: true, throws: true) 
